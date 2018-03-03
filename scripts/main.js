b45927078fdb4fba0cdd987690d7dfe2322c6225
chrisvillan <villanuevachris19@gmail.com>
2018-03-02 21:42:21 -0800

    added next and previous buttons

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,8 @@ var DETAIL_IMAGE_SELECTOR = "[data-image-role=\"target\"]";
 var DETAIL_TITLE_SELECTOR = "[data-image-role=\"title\"]";
 var THUMBNAIL_LINK_SELECTOR = "[data-image-role=\"trigger\"]";
 
+var i = 0;
+
 function setDetails(imageUrl, titleText) {
   "use strict";
   var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
@@ -18,7 +20,7 @@ function imageFromThumb(thumbnail){
 
 function titleFromThumb(thumbnail){
   "use strict";
-  return thumbnail.getAttribute('data-image-title');
+  return thumbnail.getAttribute("data-image-title");
 }
 
 function setDetailsFromThumb(thumbnail){
@@ -27,7 +29,7 @@ function setDetailsFromThumb(thumbnail){
 }
 
 function addThumbClickHandler(thumb){
-  "use strict"
+  "use strict";
   thumb.addEventListener("click",function (event){
     event.preventDefault();
     setDetailsFromThumb(thumb);
@@ -35,14 +37,50 @@ function addThumbClickHandler(thumb){
 }
 
 function getThumbnailsArray() {
-  "use strict"
+  "use strict";
   var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
   var thumbnailArray = [].slice.call(thumbnails);
   return thumbnailArray;
 }
 
+// 1. Create the button
+var button = document.createElement("left-button");
+button.innerHTML = "<-";
+
+var button2 = document.createElement("right-button");
+button2.innerHTML = "->";
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+body.appendChild(button2);
+
+
+// 3. Add event handler
+button.addEventListener ("click", function() {
+  // var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+  var thumbnails = getThumbnailsArray();
+  i=i+1;
+  if(i==5){
+    i=0;
+  }
+  setDetailsFromThumb(thumbnails[i]);
+
+});
+
+button2.addEventListener ("click", function() {
+  // var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+  var thumbnails = getThumbnailsArray();
+  i=i-1;
+  if(i==-1){
+    i=4;
+  }
+  setDetailsFromThumb(thumbnails[i]);
+
+});
+
+
 function initializeEvents() {
-  "use strict"
+  "use strict";
   var thumbnails = getThumbnailsArray();
   thumbnails.forEach(addThumbClickHandler);
 }
